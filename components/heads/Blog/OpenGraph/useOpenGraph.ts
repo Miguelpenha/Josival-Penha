d99fcfae58e40b39629c1e9c98f35a7bcfd80d02
Miguel Penha <miguel.penha1@gmail.com>
2023-12-13 17:12:38 -0300

    Test

--- components/heads/Blog/OpenGraph/useOpenGraph.ts
+++ components/heads/Blog/OpenGraph/useOpenGraph.ts
@@ -9,7 +9,7 @@ function useOpenGraph(blog: IBlog): IOpenGraph {
         description: blog.description,
         site_name: 'Instituto Educacional Josival Penha',
         url: `${process.env.NEXT_PUBLIC_DOMAIN}/blog/${blog.slug}`,
-        image: `${process.env.NEXT_PUBLIC_DOMAIN}/img/thumbnail.png`
+        image: `${process.env.NEXT_PUBLIC_DOMAIN}/${blog.image.name}`
     }
 }
 
