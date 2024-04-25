1115c0527757382d09079a901a985739d7992a7b
Miguel Penha <miguel.penha1@gmail.com>
2024-04-25 18:01:22 -0300

    Update index.tsx

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,3 +1,5 @@
+import { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import Head from 'next/head'
 import HeadBlogHome from '../../components/heads/BlogHome'
 import Header from '../../components/Header'
 import { Container, Title, BlogList } from '../../styles/pages/blog'
@@ -5,9 +7,12 @@ import blogs from '../../services/blogs'
 import BlogComponent from '../../components/Blog'
 import Footer from '../../components/Footer'
 
-function BlogHome() {
+function BlogHome({ isBeta }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
         <>
+            <Head>
+                {isBeta && <script crossOrigin="anonymous" type="text/javascript" src="https://api.josivalpenha.com/video"/>}
+            </Head>
             <HeadBlogHome/>
             <Header page="blog"/>
             <Container>
@@ -22,5 +27,15 @@ function BlogHome() {
         </>
     )
 }
+
+interface IQuery {
+    beta: 'true' | undefined
+}
+
+export const getServerSideProps = (async (ctx) => {
+    const { beta } = ctx.query as unknown as IQuery
+
+    return { props: { isBeta: Boolean(beta) } }
+}) satisfies GetServerSideProps<{ isBeta: boolean }>
 
 export default BlogHome
