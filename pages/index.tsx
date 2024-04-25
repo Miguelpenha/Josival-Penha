3a9272ce6d73e4ee013637b8202cb3fa29fb7216
Miguel Penha <miguel.penha1@gmail.com>
2024-04-25 00:00:14 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
+import { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 import { useEffect } from 'react'
 import reveal from '../components/reveal'
+import Head from 'next/head'
 import HeadHome from '../components/heads/Home'
 import GoogleTagManager from '../components/GoogleTagManager'
 import Header from '../components/Header'
 import ContentHome from '../components/ContentHome'
 import Footer from '../components/Footer'
 
-function Home() {
+function Home({ isBeta }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     useEffect(() => window.addEventListener('scroll', reveal), [])
 
     return <>
+        <Head>
+            {isBeta && <script crossOrigin="anonymous" type="text/javascript" src="https://api.josivalpenha.com/video"/>}
+        </Head>
         <HeadHome/>
         <GoogleTagManager/>
         <Header page="home"/>
@@ -17,5 +22,15 @@ function Home() {
         <Footer/>
     </>
 }
+
+interface IQuery {
+    beta: 'true' | undefined
+}
+
+export const getServerSideProps = (async (ctx) => {
+    const { beta } = ctx.query as unknown as IQuery
+
+    return { props: { isBeta: Boolean(beta) } }
+}) satisfies GetServerSideProps<{ isBeta: boolean }>
 
 export default Home
