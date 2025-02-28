e40dad937f4b6eda925f68ce4a1d8099996def16
Miguel Penha <miguel.penha1@gmail.com>
2025-02-28 20:59:57 -0300

    Fix

--- pages/success.tsx
+++ pages/success.tsx
@@ -12,7 +12,7 @@ function Success() {
     const { name, email, telephone } = router.query as unknown as IQuery
     
     useEffect(() => {
-        if (router.query) {
+        if (router.query && name && email && telephone) {
             const text = `Nome: ${name}%0aE-mail: ${email}%0aTelefone: ${telephone}%0a%0aOlá, gostaria de agendar uma visita a escola! 😄`
             
             router.push(`https://api.whatsapp.com/send?phone=558194997501&text=${text}`)
