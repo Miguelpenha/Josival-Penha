3d40b1ae2edbcd2a825b725e2d8b23469c033d41
Miguel Penha <miguel.penha1@gmail.com>
2025-11-24 14:58:21 -0300

    Update General.tsx

--- components/heads/Global/General.tsx
+++ components/heads/Global/General.tsx
@@ -8,6 +8,19 @@ function General() {
             <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <script crossOrigin="anonymous" type="text/javascript" src="https://api.josivalpenha.com/nyxel/video"/>
+            <script dangerouslySetInnerHTML={{
+                __html: `var _paq = window._paq = window._paq || [];
+                /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                _paq.push(['trackPageView']);
+                _paq.push(['enableLinkTracking']);
+                (function() {
+                    var u="//matomo.nyxel.tech/";
+                    _paq.push(['setTrackerUrl', u+'matomo.php']);
+                    _paq.push(['setSiteId', '1']);
+                    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+                })();`
+            }}/>
         </Head>
     )
 }
